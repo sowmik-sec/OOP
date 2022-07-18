2bbe9ca2613ed8a22274f8556383067d23c20b21
sowmik-sec <48240954+sowmik-sec@users.noreply.github.com>
2022-07-18 16:21:53 +0600

    encapsulation done Alhamdulillah

---
+++ encapsulation/Test.java
@@ -0,0 +1,11 @@
+package oop.encapsulation;
+
+public class Test {
+    public static void main(String[] args) {
+        Person p = new Person();
+        p.setName("Ahsan Habib Sowmik");
+        System.out.println(p.getName());
+        p.setAge(26);
+        System.out.println(p.getAge());
+    }
+}
